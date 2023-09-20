2f07643cb4a8665807c13195b70d758de008b760
AnaOliiveira <oliveira.miranda2102@gmail.com>
2023-09-20 11:13:25 -0300

    Correção e Atualização do código

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from blog.views import (
-    index, ola, post_show,
+    index, ola, post_show, PostDetailView,
     get_all_posts, get_post,
-    PostCreateView )
+    PostCreateView, create_post, PostListView, SobreTemplateView, )
+
 
 
 urlpatterns = [
@@ -11,12 +12,17 @@ urlpatterns = [
     path('index/', index, name= "index"),
     path('ola/', ola, name= "ola"),
     path('posts/all', ola, name="posts_list"),
-    path('posts/all', ola, name="posts_list"),
     path('post/<int:post_id>', post_show, name="exibe_post"),
-    # path('post/<int:pk>/show', PostDetailView.as_view(), name="post_detail"),
+    path('post/<int:pk>/show', PostDetailView.as_view(), name="post_detail"),
     path('api/posts', get_all_posts, name="posts_data"),
     path('api/posts/<int:post_id>', get_post, name="post_data"),
     path('api/posts', get_all_posts, name="posts_data"),
     path('api/posts/<int:post_id>', get_post, name="post_data"),
     path('post/add', PostCreateView.as_view(), name="post_add"),
+    path('api/post/add', create_post, name="create_post_data"),
+    path('posts', PostListView.as_view(), name="posts_all"),
+    path('about-us',
+            SobreTemplateView.as_view(),
+            name="about_page"
+    ),
 ]
